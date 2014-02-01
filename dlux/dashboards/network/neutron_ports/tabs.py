bce8361a0d2bfecfef60266db48b4510b6719e04
Dave Tucker <dave@dtucker.co.uk>
2014-02-01 03:39:59 +0000

    OVS Information added to Port Detail Page

--- dlux/dashboards/network/neutron_ports/tabs.py
+++ dlux/dashboards/network/neutron_ports/tabs.py
@@ -21,6 +21,60 @@ from horizon import tabs
 
 from dlux.api import get_client
 
+def get_ovs_ids(client, port_id):
+
+    ovs_interface = _get_interface_id(client, port_id)
+    ovs_port = _get_port_id(client, ovs_interface)
+    ovs_bridge = _get_bridge_id(client, ovs_port)
+
+    return { "interface_id" : ovs_interface,
+             "port_id" : ovs_port,
+             "bridge_id": ovs_bridge }
+
+
+def _get_interface_id(client, port_id):
+    connections = client.connection_manager.list()
+    for n in connections:
+        if n.type == 'OVS':
+            data = client.ovsdb.list(n.type, n.id, 'interface')
+            rows = data["rows"]
+            for row in rows:
+                uuid = row
+                for items in rows[uuid]["external_ids"]:
+                    if type(items) == list:
+                        for item in items:
+                            if item[0] == "iface-id":
+                                if item[1] == port_id:
+                                    return uuid
+
+def _get_port_id(client, interface_id):
+    connections = client.connection_manager.list()
+    for n in connections:
+        if n.type == 'OVS':
+            data = client.ovsdb.list(n.type, n.id, 'port')
+            rows = data["rows"]
+            for row in rows:
+                uuid = row
+                for items in rows[uuid]["interfaces"]:
+                    if type(items) == list:
+                        for item in items:
+                            if item[1] == interface_id:
+                                return uuid
+
+def _get_bridge_id(client, port_id):
+    connections = client.connection_manager.list()
+    for n in connections:
+        if n.type == 'OVS':
+            data = client.ovsdb.list(n.type, n.id, 'bridge')
+            rows = data["rows"]
+            for row in rows:
+                uuid = row
+                for items in rows[uuid]["ports"]:
+                    if type(items) == list:
+                        for item in items:
+                            if item[1] == port_id:
+                                return uuid
+
 
 class OverviewTab(tabs.Tab):
     name = _("Overview")
@@ -31,7 +85,10 @@ class OverviewTab(tabs.Tab):
         port_id = self.tab_group.kwargs['port_id']
         client = get_client(self.request)
         port = client.neutron.ports.get(port_id)
-        return {'port': port}
+
+        data = get_ovs_ids(client, port_id)
+        data["port"] = port
+        return data
 
 
 class PortDetailTabs(tabs.TabGroup):
