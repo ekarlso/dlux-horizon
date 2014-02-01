bc9f5bdbd9afeb1a8b11f90858138ce1a495e79e
Dave Tucker <dave@dtucker.co.uk>
2014-02-01 01:06:13 +0000

    Neutron Network Details Page

--- dlux/dashboards/network/neutron_networks/tables.py
+++ dlux/dashboards/network/neutron_networks/tables.py
@@ -18,10 +18,23 @@ from django.core import urlresolvers
 from django.utils.translation import ugettext_lazy as _
 from horizon import tables
 
+from dlux.utils.filters import keys_as_id
+
 STATES = {0: 'DOWN', 1: 'UP'}
 
+def get_network_link(datum):
+    view = "horizon:network:neutron_networks:detail"
+    if datum.id:
+        #ToDo: Not sure why this works, but it does
+        link = urlresolvers.reverse(view, args={datum.id})
+    else:
+        link = None
+    return link
+
 class NeutronNetworksTable(tables.DataTable):
-    id = tables.Column('id', verbose_name=_('Identifier'))
+    id = tables.Column('id',
+                       verbose_name=_('Identifier'),
+                       link=get_network_link)
     name = tables.Column('name', verbose_name=_('Name'))
     tenant_id = tables.Column('tenant_id', verbose_name=_("Tenant Identifier"))
     type = tables.Column('provider:network_type',
@@ -33,6 +46,9 @@ class NeutronNetworksTable(tables.DataTable):
     class Meta:
         name = 'neutron_networks'
         verbose_name = _('Neutron Networks')
+
+    def get_object_id(self, datum):
+        return datum.id
 
 class NeutronNetworkDetailTable(tables.DataTable):
     id = tables.Column('id', verbose_name=_('Identifier'))
